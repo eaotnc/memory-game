cc5d6f16cfa0cfb269db020b42296266facfc441
Eao Thanatcha <bonsai.eao@gmail.com>
2025-01-22 00:57:21 +0700

    feat: implement  card container component

---
+++ frontend/src/components/CardContainer/CardContainer.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { Card } from "../../interfaces";
+import "./CardContainer.css";
+
+const CardContainer = () => {
+  const [cards, setCards] = useState<Card[]>([
+    { key: 1, id: 1, imageUrl: "https://picsum.photos/id/237/200/300" },
+    { key: 2, id: 2, imageUrl: "https://picsum.photos/id/238/200/300" },
+    { key: 3, id: 3, imageUrl: "https://picsum.photos/id/239/200/300" },
+    { key: 4, id: 4, imageUrl: "https://picsum.photos/id/240/200/300" },
+    { key: 5, id: 5, imageUrl: "https://picsum.photos/id/241/200/300" },
+    { key: 6, id: 6, imageUrl: "https://picsum.photos/id/242/200/300" },
+    { key: 7, id: 7, imageUrl: "https://picsum.photos/id/243/200/300" },
+    { key: 8, id: 8, imageUrl: "https://picsum.photos/id/244/200/300" },
+    { key: 9, id: 1, imageUrl: "https://picsum.photos/id/237/200/300" },
+    { key: 10, id: 2, imageUrl: "https://picsum.photos/id/238/200/300" },
+    { key: 11, id: 3, imageUrl: "https://picsum.photos/id/239/200/300" },
+    { key: 12, id: 4, imageUrl: "https://picsum.photos/id/240/200/300" },
+    { key: 13, id: 5, imageUrl: "https://picsum.photos/id/241/200/300" },
+    { key: 14, id: 6, imageUrl: "https://picsum.photos/id/242/200/300" },
+    { key: 15, id: 7, imageUrl: "https://picsum.photos/id/243/200/300" },
+    { key: 16, id: 8, imageUrl: "https://picsum.photos/id/244/200/300" },
+  ]);
+
+  return (
+    <div className="card-container">
+      {cards.map((card) => (
+        <div key={card.id} className="card">
+          <img className="rounded-md" key={card.id} src={card.imageUrl} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardContainer;
---
+++ frontend/src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export interface Card {
+  key: number;
+  id: number;
+  imageUrl: string;
+}
