8e4903bb54b566414cc90b2a6c2f4b7cb12dac3e
Eao Thanatcha <bonsai.eao@gmail.com>
2025-01-23 02:58:33 +0700

    feat: add Prisma module and service for database integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardsModule } from './cards/cards.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CardsModule],
-  controllers: [AppController],
+  imports: [CardsModule, PrismaModule],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
